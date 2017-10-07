3507d1b1f3c44250ad966ce1c1fa7c9cb3e793f0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-10-07 18:36:55 +0200

    Moved main to app class

--- samples/ColorBlenderAvalonia/App.xaml.cs
+++ samples/ColorBlenderAvalonia/App.xaml.cs
@@ -1,12 +1,32 @@
 ﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Avalonia;
+using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
+using Serilog;
 
 namespace ColorBlenderAvalonia
 {
     public class App : Application
     {
+        static void Main(string[] args)
+        {
+            InitializeLogging();
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+
+        static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
+#endif
+        }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
