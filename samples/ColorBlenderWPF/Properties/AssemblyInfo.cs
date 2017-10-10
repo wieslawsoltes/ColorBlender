3621d0105b2c0e7832724ce0f34e815437189074
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2017-10-10 17:47:27 +0200

    Updated assembly information

--- samples/ColorBlenderWPF/Properties/AssemblyInfo.cs
+++ samples/ColorBlenderWPF/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 using System.Windows;
 
 [assembly: AssemblyTitle("ColorBlenderWPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A sample WPF application for .NET library for color matching and palette design.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wiesław Šoltés")]
 [assembly: AssemblyProduct("ColorBlenderWPF")]
 [assembly: AssemblyCopyright("Copyright © Wiesław Šoltés 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
